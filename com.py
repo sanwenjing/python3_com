501901491f7b55e3b3b5f2914c3105b69f58b514
sanwenjing <sanwenjing@163.com>
2024-03-03 09:19:47 +0800

    download add

--- com.py
+++ com.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-import os,sys,time,random;
+import os,sys,time,random,requests
 import datetime as dt;
 import platform as pf;
 import configparser  #ini
@@ -164,14 +164,21 @@ def mkDir(dirName):
 # 检查input文件夹是否存在
   if not os.path.exists(dirName):
     os.makedirs(dirName)
-
+def download(fileurl,title,dir="download\\"):#title如 XXX.mp4
+  mkDir(dir)#建目录
+  file_content = requests.get(url=fileurl).content
+  with open(dir + title, mode='wb') as f:
+    f.write(file_content)
 
 if __name__=="__main__":
          #log1=log();
          #log1.w("test");
-         conf1=Config();
+         #conf1=Config();
          #print conf1.getFd();
-         conf1.set("test","123456nihao")
-         print(conf1.get("test"))
-         mkDir(".\\input")
+         #conf1.set("test","123456nihao")
+         #print(conf1.get("test"))
+         #mkDir(".\\input")
+         #useAliyunSource()
+         download("https://vdept3.bdstatic.com/mda-qc20br3dig0x0bg5/sc/cae_h264/1709424976869467322/mda-qc20br3dig0x0bg5.mp4?v_from_s=hkapp-haokan-nanjing&auth_key=1709438932-0-0-aadb6d7851b811493c024a27c958dcf6&bcevod_channel=searchbox_feed&cr=2&cd=0&pd=1&pt=3&logid=0532247206&vid=10254206589342494431&klogid=0532247206&abtest=116096_1","荣庵老师为粉丝、学员讲解 ——享荫福厚 才学富足 荣庵老师 婚期择吉 易学 传承和弘扬民族文化 弘扬传统文化 荣庵讲易 十二生肖 智慧人生"+".mp4")
+
          #print getHtml("www.baidu.com")         
